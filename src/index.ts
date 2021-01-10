1dc23843772073b9f13f231f81b79283c722ee2d
chamlokuge <chamodilokuge@gmail.com>
2021-01-10 22:43:05 +0530

    Modify index.ts file

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,19 @@ import cors from 'cors';
 import { dbConnect } from './utilities/dbConnection' ;
 import { noteRouter } from './routes/noteRoutes';
 import dotenv from 'dotenv';
+import { checkToken } from './utilities/manageWebTokens';
 
 dotenv.config();
 const app = express();
-//const port = process.env.PORT || 4000
-
-// dbConnect();
-
-// app.listen(port, () => {
-//     console.log('Server started on port ' +port)
-// });
-
-
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-// console.log("aaaaaaaa")
-
-
-
 app.get('/', (req, res) => res.send('Welcome to NodeJs App using TypeScript'));
 
-app.use('/notes', noteRouter);
-app.use('/general',loginRegistrationRouter);
+app.use('/api/v1/notes', checkToken, noteRouter);
+app.use('/api/v1/auth',loginRegistrationRouter);
 
 app.listen(process.env.PORT, async () => {
     console.log(`server started on port ${process.env.PORT}`);
