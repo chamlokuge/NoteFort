8de0ddd00c46a7cec11589e7deab01fcc3d82d61
chamlokuge <chamodilokuge@gmail.com>
2021-01-10 12:57:55 +0530

    Modify utilities

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
+import { loginRegistrationRouter } from './routes/loginRegistrationRoutes';
 import express from 'express';
 import * as bodyParser from 'body-parser';
 import cors from 'cors';
 import { dbConnect } from './utilities/dbConnection' ;
+import { noteRouter } from './routes/noteRoutes';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 4000
+//const port = process.env.PORT || 4000
 
-dbConnect();
+// dbConnect();
 
-app.listen(port, () => {
-    console.log('Server started on port ' +port)
-});
+// app.listen(port, () => {
+//     console.log('Server started on port ' +port)
+// });
+
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,4 +24,14 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 // console.log("aaaaaaaa")
 
+
+
 app.get('/', (req, res) => res.send('Welcome to NodeJs App using TypeScript'));
+
+app.use('/notes', noteRouter);
+app.use('/general',loginRegistrationRouter);
+
+app.listen(process.env.PORT, async () => {
+    console.log(`server started on port ${process.env.PORT}`);
+    await dbConnect(process.env.DBLINK);
+})
